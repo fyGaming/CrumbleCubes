2cde9721c1c826b3d8a04d6c6ff218aff1c4f4a5
fyGaming <13501355311@126.com>
2019-02-18 07:38:44 +0800

    Added cube generator

---
+++ CrumbleCubes/Assets/Scripts/CubeGenerator.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeGenerator : MonoBehaviour
+{
+    void GenerateCubes()
+    {
+        int x = 0;
+        for (int _x = 0; _x < 20; _x++)
+        {
+            int z = 0;
+            for (int _z = 0; _z < 20; _z++)
+            {
+                GameObject gameObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                MeshRenderer meshRenderer = gameObject.GetComponent<MeshRenderer>();
+                meshRenderer.material.shader = Shader.Find("Standard");
+                meshRenderer.material.SetColor("_Color", Color.white);
+                gameObject.transform.localScale = new Vector3(1f,1f,1f);
+                gameObject.transform.position = new Vector3(x, 0, z);
+                z++;
+                Debug.Log(z);
+            }
+            x++;
+            Debug.Log(x);
+        }
+
+    }
+
+    private void Start()
+    {
+        GenerateCubes();
+    }
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            GenerateCubes();
+        }
+    }
+
+
+}
